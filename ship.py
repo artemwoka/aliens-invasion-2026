cf2ae4adf098db9eff4fab4930917aff136885ae
artemwoka <artemwoka@gmail.com>
2026-01-31 14:58:03 +0100

    Add borders left and right

--- ship.py
+++ ship.py
@@ -30,9 +30,9 @@ class Ship:
     
     def update(self):
         """Оновлює позицію корабля залежно від флагів переміщення."""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         #Оновлює об'єкт rect на основі self.x
