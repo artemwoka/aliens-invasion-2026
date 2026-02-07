a091a63605068e086d4af5995d1d5a90368eb313
artemwoka <artemwoka@gmail.com>
2026-02-07 15:40:57 +0100

    Add aliens update

--- alien.py
+++ alien.py
@@ -9,6 +9,7 @@ class Alien(sprite.Sprite):
         """Ініціалізує прибульця та встановлює його початкову позицію."""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Завантажує зображення прибульця та отримує його rect.
         self.image = pg.image.load('images/alien.bmp')
@@ -20,3 +21,8 @@ class Alien(sprite.Sprite):
 
         # Зберігає точну горизонтальну позицію прибульця.
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Рухає прибульця вправо."""
+        self.x += self.settings.alien_speed
+        self.rect.x = self.x
