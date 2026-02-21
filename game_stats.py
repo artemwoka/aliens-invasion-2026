154f69d894636a1071540d740397aab0e4a5473d
artemwoka <artemwoka@gmail.com>
2026-02-21 15:21:18 +0100

    Add Scoreboard class

--- game_stats.py
+++ game_stats.py
@@ -12,3 +12,4 @@ class GameStats:
     def reset_stats(self):
         """Ініціалізує статистику, яка може змінюватися впродовж гри."""
         self.ships_left = self.settings.ship_limit
+        self.score = 0
